ae77ab9ec3fdc0dd69380f2b14f111af3b2cfa21
InGyu Moon <ansdlsrb1121@naver.com>
2024-05-24 11:02:01 +0900

    fix: 메인페이지 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,8 @@
 export default function page(){
     return(
         <div>
-            <h1>KOU HOUSE 메인페이지</h1>
+            <h1>KOUSE 메인페이지</h1>
             <h3>상단 네비게이션에서 원룸으로 들어가 방 정보를 확인하세요.</h3>
-            <h1>hello</h1>
-            <h1>hello</h1>
-            <h1>hello</h1>
         </div>
     )
     
